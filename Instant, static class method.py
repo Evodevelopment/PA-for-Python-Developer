48512f91c0556622b46e7c1fea15aa16c756c918
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-15 17:21:15 +0200

    Update Instant, static class method.py

--- Instant, static class method.py
+++ Instant, static class method.py
@@ -1,1 +1,16 @@
+#Instant method
+class MyClass:
+    def instance_method(self):
+        # Accessing instance variables
+        self.instance_variable = 10
+        
+        # Accessing class variable
+        MyClass.class_variable = 20
+        
+        # Perform operations specific to an instance
+        # ...
+        
+obj = MyClass()  # Creating an instance of the class
+obj.instance_method()  # Calling the instance method on the instance
 
+#Static method
